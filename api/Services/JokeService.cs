1ae179ea34da95047506b4fdd31ee9bc85bd785f
reall <reall@MSI>
2024-11-15 00:00:21 -0700

    added to the service so we can get a random joke and edit a joke

--- api/Services/JokeService.cs
+++ api/Services/JokeService.cs
@@ -12,4 +12,28 @@ public class JokeService{
         var jokeList = await context.Jokes.ToListAsync();
         return jokeList;
     }
+
+    public async Task<Joke> GetRandomJokeAsync() {
+        var context = contextFactory.CreateDbContext();
+        var jokeList = await context.Jokes.ToListAsync();
+        int rand = Random.Shared.Next(0, jokeList.Count);
+        return jokeList[rand];
+    }
+    public async Task<Joke> EditJokeAsync(int id, Joke updatedJoke) {
+        var context = contextFactory.CreateDbContext();
+        var dbjoke = await context.Jokes.Where(j => j.id == id).FirstOrDefaultAsync();
+        if (dbjoke is not null) {
+            dbjoke.answer = updatedJoke.answer;
+            dbjoke.author = updatedJoke.author;
+            dbjoke.question = updatedJoke.question;
+            await context.SaveChangesAsync();
+            return dbjoke;
+        }
+        else {
+            //negative 1 status to indicate error. Or, should I throw an exception?
+            return new Joke {
+                id = -1
+            };
+        }
+    }
 }

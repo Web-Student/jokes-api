cf3cf00d7f1d8b8929821926e25a5b2cbd6f304b
reall <reall@MSI>
2024-12-03 11:22:45 -0700

    altered init.sql

--- database/init.sql
+++ database/init.sql
@@ -12,12 +12,19 @@ INSERT INTO joke VALUES
 ,(2, 'test@email.com', 'Dad, can you tell me what a solar eclipse is?', 'No son')
 ,(3, 'rachel.allen1@students.snow.edu', 'Have you ever eaten a clock?', 'It''s very time-consuming');
 
-CREATE SEQUENCE public.joke_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+
+DO $$ BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_class WHERE relname = 'joke_id_seq') THEN
+    CREATE SEQUENCE joke_id_seq START WITH 1 INCREMENT BY 1;
+  END IF;
+END $$;
+
+-- CREATE SEQUENCE public.joke_id_seq
+--     AS integer
+--     START WITH 1
+--     INCREMENT BY 1
+--     NO MINVALUE
+--     NO MAXVALUE
+--     CACHE 1;
 
 ALTER SEQUENCE public.joke_id_seq OWNED BY public.joke.Id;
